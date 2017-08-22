a69fe4cd132b81872edfa52280e8c525f5823206
JUNHYEONG\Junhyeong <wnsgud0750@gmail.com>
2017-08-22 19:31:24 +0900

    팝업 띄우..기..

---
+++ app/src/main/java/com/example/wnsgu/myapplication/PopupActivity.java
@@ -0,0 +1,108 @@
+package com.example.wnsgu.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+/**
+ * Created by Junhyeong on 2017-08-22.
+ */
+
+public class PopupActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_popup);
+    }
+
+    public void onClick(View view) {
+        Button localId = (Button) findViewById(R.id.local1);
+        String local;
+        Intent intent = new Intent(this, Activity2.class);
+        switch (view.getId())
+        {
+            case R.id.local1:
+                localId = (Button) findViewById(R.id.local1);
+                break;
+            case R.id.local2:
+                localId = (Button) findViewById(R.id.local2);
+                break;
+            case R.id.local3:
+                localId = (Button) findViewById(R.id.local3);
+                break;
+            case R.id.local4:
+                localId = (Button) findViewById(R.id.local4);
+                break;
+            case R.id.local5:
+                localId = (Button) findViewById(R.id.local5);
+                break;
+            case R.id.local6:
+                localId = (Button) findViewById(R.id.local6);
+                break;
+            case R.id.local7:
+                localId = (Button) findViewById(R.id.local7);
+                break;
+            case R.id.local8:
+                localId = (Button) findViewById(R.id.local8);
+                break;
+            case R.id.local9:
+                localId = (Button) findViewById(R.id.local9);
+                break;
+            case R.id.local10:
+                localId = (Button) findViewById(R.id.local10);
+                break;
+            case R.id.local11:
+                localId = (Button) findViewById(R.id.local11);
+                break;
+            case R.id.local12:
+                localId = (Button) findViewById(R.id.local12);
+                break;
+            case R.id.local13:
+                localId = (Button) findViewById(R.id.local13);
+                break;
+            case R.id.local14:
+                localId = (Button) findViewById(R.id.local14);
+                break;
+            case R.id.local15:
+                localId = (Button) findViewById(R.id.local15);
+                break;
+            case R.id.local16:
+                localId = (Button) findViewById(R.id.local16);
+                break;
+            case R.id.local17:
+                localId = (Button) findViewById(R.id.local17);
+                break;
+            case R.id.local18:
+                localId = (Button) findViewById(R.id.local18);
+                break;
+            case R.id.local19:
+                localId = (Button) findViewById(R.id.local19);
+                break;
+            case R.id.local20:
+                localId = (Button) findViewById(R.id.local20);
+                break;
+            case R.id.local21:
+                localId = (Button) findViewById(R.id.local21);
+                break;
+            case R.id.local22:
+                localId = (Button) findViewById(R.id.local22);
+                break;
+            case R.id.local23:
+                localId = (Button) findViewById(R.id.local23);
+                break;
+            case R.id.local24:
+                localId = (Button) findViewById(R.id.local24);
+                break;
+            case R.id.local25:
+                localId = (Button) findViewById(R.id.local25);
+                break;
+        }
+        local = localId.getText().toString();
+        intent.putExtra("local", local);
+        startActivity(intent);
+    }
+}
